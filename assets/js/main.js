9a361cc1ae80bda9c897097c355a644f3289dcfa
Guilherme Corrêa <108332661+guicorrea93@users.noreply.github.com>
2025-12-27 10:33:56 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
+const topbarHTML = `
+  <nav class="nav">
+    <div class="brand">Guilherme <b>Corrêa</b></div>
+    <div class="menu">
+      <a href="index.html">Início</a>
+      <a href="projetos.html">Projetos</a>
+    </div>
+  </nav>
+`;
 
+const el = document.getElementById('topbar');
+if (el) el.innerHTML = topbarHTML;
